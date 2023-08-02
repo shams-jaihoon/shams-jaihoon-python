e86d9f858bbd7e7b11a30fec7b50d2a4af19847e
shamsurahman-jaihoon <shamsurahman.jaihoon@gmail.com>
2023-08-02 07:03:48 -0700

    task modified

--- task#13/defaultValue.py
+++ task#13/defaultValue.py
@@ -2,13 +2,7 @@ def print_message(message, repeat_count=1):
     for x in range(repeat_count):
         print(message)
 
-    user = input("Enter a message")
-    user_count = input("inter a repeat count (optional):")
+user = input("Enter a message")
+user_count = input("inter a repeat count (optional):")
 
-    if user and user_count:
-        print_message(user, int(user_count))
-    else:
-        print_message(user)
-
-
-print_message("Enter a message")
+print_message(user, int(user_count))
